49fc040ce11fc3f728b1a478dcfd91f85c29462e
marcsikor <s223464@student.uek.krakow.pl>
2022-06-03 20:14:12 +0200

    2022-06-03 MS

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,7 @@ import requests
 import pandas as pd
 from matplotlib import pyplot as plt
 from bs4 import BeautifulSoup
-from flask import render_template
+from flask import render_template, redirect, url_for, request
 
 def extract_element(ancestor, selector, attribute=None, return_list=False):
     try:
@@ -32,42 +32,53 @@ review_elements = {
 }
 
 @app.route('/')
-@app.route('/index')
 def index():
     return render_template("index.html.jinja")
 
-@app.route('/extract/<product_id>')
-def extract(product_id):
-    url = f"https://www.ceneo.pl/{product_id}#tab=reviews"
-    all_reviews = []
-    while(url):
-        response = requests.get(url)
-        page_dom = BeautifulSoup(response.text, 'html.parser')
-        reviews = page_dom.select("div.js_product-review")
-        for review in reviews:
-            single_review = {key:extract_element(review, *values)
-                            for key, values in review_elements.items()}
-            single_review["review_id"] = review["data-entry-id"]
+@app.route('/author')
+def author():
+    return render_template("author.html.jinja")
 
-            single_review["recommendation"] = True if single_review["recommendation"] == "Polecam" else False if single_review["recommendation"] == "Nie polecam" else None
-            single_review["stars"] = float(single_review["stars"].split("/").pop(0).replace(",", "."))
-            single_review["useful"] = int(single_review["useful"])
-            single_review["useless"] = int(single_review["useless"])
-            single_review["publish_date"] = single_review["publish_date"].split(" ").pop(0) if single_review["publish_date"] is not None else None
-            single_review["purchase_date"] = single_review["purchase_date"].split(" ").pop(0) if single_review["purchase_date"] is not None else None
-            all_reviews.append(single_review)
-        try: 
-            next_page = page_dom.select_one("a.pagination__next")
-            url = "https://www.ceneo.pl"+next_page["href"]
-        except TypeError: url = None
+@app.route('/extract', methods=["POST", "GET"])
+def extract():
+    if request.method == "POST":
+        product_id = request.form.get("product_id")
+        url = f"https://www.ceneo.pl/{product_id}#tab=reviews"
+        all_reviews = []
+        while(url):
+            response = requests.get(url)
+            page_dom = BeautifulSoup(response.text, 'html.parser')
+            reviews = page_dom.select("div.js_product-review")
+            for review in reviews:
+                single_review = {key:extract_element(review, *values)
+                                for key, values in review_elements.items()}
+                single_review["review_id"] = review["data-entry-id"]
 
-    with open(f"./app/reviews/{product_id}.json", "w", encoding="UTF-8") as f:
-        json.dump(all_reviews, f, indent=4, ensure_ascii=False)
-    return json.dumps(all_reviews, indent=4, ensure_ascii=False)
+                single_review["recommendation"] = True if single_review["recommendation"] == "Polecam" else False if single_review["recommendation"] == "Nie polecam" else None
+                single_review["stars"] = float(single_review["stars"].split("/").pop(0).replace(",", "."))
+                single_review["useful"] = int(single_review["useful"])
+                single_review["useless"] = int(single_review["useless"])
+                single_review["publish_date"] = single_review["publish_date"].split(" ").pop(0) if single_review["publish_date"] is not None else None
+                single_review["purchase_date"] = single_review["purchase_date"].split(" ").pop(0) if single_review["purchase_date"] is not None else None
+                all_reviews.append(single_review)
+            try: 
+                next_page = page_dom.select_one("a.pagination__next")
+                url = "https://www.ceneo.pl"+next_page["href"]
+            except TypeError: url = None
+        if not os.path.exists("app/reviews"):
+            os.makedirs("app/reviews")
+        with open(f"./app/reviews/{product_id}.json", "w", encoding="UTF-8") as f:
+            json.dump(all_reviews, f, indent=4, ensure_ascii=False)
+        return redirect(url_for('product', product_id=product_id))
+    else:
+        return render_template("extract.html.jinja")
 
 @app.route('/products')
 def products():
-    products = [item.split(".").pop(0) for item in os.listdir("app/reviews")]
+    if os.path.exists("app/reviews"):
+        products = [item.split(".").pop(0) for item in os.listdir("app/reviews")]
+    else:
+        products = []
     return render_template("products.html.jinja", products=products)
 
 @app.route('/product/<product_id>')
@@ -79,8 +90,10 @@ def product(product_id):
         "pros_count": reviews.pros.map(bool).sum(),
         "cons_count": reviews.cons.map(bool).sum()
     }
+    if not os.path.exists("app/static/plots"):
+        os.makedirs("app/static/plots")
     recommendations = reviews.recommendation.value_counts(dropna = False)
     recommendations.plot.pie()
     plt.savefig(f"app/static/plots/{product_id}_recommendation.png")
     plt.close()
-    return render_template("product.html.jinja", stats=stats, product_id=product_id)
+    return render_template("product.html.jinja", stats=stats, product_id=product_id, tables=[reviews.to_html(classes='table table-sm table-hover table-bordered', header="true", table_id="reviews")])
